31f48bfceeae965e1679dd1b4d9c9e1bd151e578
chris-daynes <chrisdaynes@gmail.com>
2016-10-13 10:00:06 +1300

    added the x axis mouse mvt

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var canvas,
     ballY = 0;
     ballSpeedY = 8;
     paddleX = 200;
+    paddleWidth = 100;
 
 window.onload = function(){
   canvas = document.getElementById('game');
@@ -13,16 +14,22 @@ window.onload = function(){
       ballMove();
   }, 1000/framesPerSecond);
 
-  canvas.addEventListener('keydown', movePaddle);
+  canvas.addEventListener('mousemove', function(e){
+    var mousePos = calculateMousePos(e);
+    paddleX = mousePos.x - paddleWidth/2;
+  });
 };
 
-function movePaddle(e){
-  if(e.which === 39) {
-    paddleX += 6;
-  }
-  if(e.which === 37){
-    paddleX -= 6;
-  }
+
+function calculateMousePos(e){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = e.clientX -rect.left - root.scrollLeft;
+  var mouseY = e.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY
+  };
 }
 
 function ballMove (){
@@ -37,7 +44,7 @@ function ballMove (){
 
 function drawEverything() {
   rectangle(0, 0, canvas.width, canvas.height, 'black'); //Fill in the canvas
-  rectangle(paddleX, 490, 100, 10, 'white'); //draw bottom paddle
+  rectangle(paddleX, 490, paddleWidth, 10, 'white'); //draw bottom paddle
   ball(250, ballY, 10, 'white'); //draw a white ball.
 }
 
