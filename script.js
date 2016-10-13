9b1ea610d51ca7521fd19755e8453883d81f2fee
chris-daynes <chrisdaynes@gmail.com>
2016-10-13 22:21:52 +1300

    added fonts

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var canvas,
-    ctx,
-    ballY = 0;
-    ballSpeedY = 8;
-    paddleX = 200;
-    paddleWidth = 100;
+
+var ctx = document.getElementById('game').getContext('2d');
+var ballY = 0, ballX = ctx.canvas.width/2;
+var ballSpeedY = 10, ballSpeedX = 10;
+var paddleX = 200; //x position of paddle
+var paddleWidth = 100;
+var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
 
 window.onload = function(){
-  canvas = document.getElementById('game');
-  ctx = canvas.getContext('2d');
-  var framesPerSecond = 50;
-  setInterval(function (){
-      drawEverything();
-      ballMove();
-  }, 1000/framesPerSecond);
 
-  canvas.addEventListener('mousemove', function(e){
+  function animate(){
+    ctx.save();
+    ctx.clearRect(0,0,cW, cH);
+
+    drawEverything();
+    ballMove();
+
+    ctx.restore();
+  }
+  var animateInterval = setInterval(animate, 30);
+
+
+  ctx.canvas.addEventListener('mousemove', function(e){
     var mousePos = calculateMousePos(e);
     paddleX = mousePos.x - paddleWidth/2;
   });
@@ -22,10 +29,8 @@ window.onload = function(){
 
 
 function calculateMousePos(e){
-  var rect = canvas.getBoundingClientRect();
-  var root = document.documentElement;
-  var mouseX = e.clientX -rect.left - root.scrollLeft;
-  var mouseY = e.clientY - rect.top - root.scrollTop;
+  var mouseX = e.clientX -ctx.canvas.offsetLeft;
+  var mouseY = e.clientY - ctx.canvas.offsetTop;
   return {
     x: mouseX,
     y: mouseY
@@ -33,28 +38,50 @@ function calculateMousePos(e){
 }
 
 function ballMove (){
+  //these two lines changes ball position incrementally by ballSpeed.
+  ballX += ballSpeedX;
   ballY += ballSpeedY;
-    if(ballY > canvas.height){
-      ballSpeedY = -ballSpeedY;
+    //here if the ball goes beyound the baseline.
+    if(ballY > ctx.canvas.height){
+      if(ballX>paddleX && ballX<paddleX+paddleWidth){
+        //change ball velocity if hit on the angle
+        var deltaX = ballX - (paddleX + paddleWidth/2);
+        ballSpeedX = deltaX * 0.35;
+        ballSpeedY = -ballSpeedY;
+      }
+      else {
+        resetBall();
+      }
     }
     if(ballY < 0){
       ballSpeedY = -ballSpeedY;
     }
+    if (ballX > ctx.canvas.width) {
+      ballSpeedX = -ballSpeedX;
+    }
+    if (ballX < 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+}
+
+function resetBall(){
+  ballY = 0;
+  ballX = cW/2;
 }
 
 function drawEverything() {
-  rectangle(0, 0, canvas.width, canvas.height, 'black'); //Fill in the canvas
+  rectangle(0, 0, ctx.canvas.width, ctx.canvas.height, 'black'); //Fill in the canvas
   rectangle(paddleX, 490, paddleWidth, 10, 'white'); //draw bottom paddle
-  ball(250, ballY, 10, 'white'); //draw a white ball.
+  ball(ballX, ballY, 10, 'yellow'); //draw a white ball.
 }
 
-//function constructor to draw a rectangle
+//function constructor to draw a rectangles
 function rectangle(x, y, width, height, color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 }
 
-//function constructor to make a circle
+//function constructor to make a circles
 function ball (x, y, radius, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
